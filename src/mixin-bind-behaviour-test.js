4579951dacb9dd5f260700f6e035e076f46ce023
Matt Chaffe <matt@mattchaffe.uk>
2019-08-14 12:40:36 +0100

    additional test for fromAttribute to ensure no duplicate property set

--- src/mixin-bind-behaviour-test.js
+++ src/mixin-bind-behaviour-test.js
@@ -47,4 +47,63 @@ if (browserSupports.customElements) {
 		el.setAttribute('lname', 'Meyer');
 		assert.equal(el.lname, 'Meyer', 'We have set the property from the attribute');
 	});
+
+	QUnit.test("properties are not set when attribute does not exist", function(assert) {
+		class BasicBindingsElement extends StacheElement {
+			static get view() {
+				return `<h1>{{name}}</h1>`;
+			}
+
+			static get props() {
+				return {
+					name: {
+						type: type.maybeConvert(String),
+						bind: fromAttribute,
+						set (v) {
+							return v;
+						}
+					}
+				};
+			}
+		}
+		customElements.define("no-attribute", BasicBindingsElement);
+
+		const el = document.createElement('no-attribute');
+		fixture.appendChild(el);
+		
+		assert.strictEqual(el.getAttribute('name'), null, 'We have not initialized the attribute');
+		assert.strictEqual(el.name, undefined, 'We have not initialized the property');
+	});
+
+	QUnit.test("property is not called multiple times", function(assert) {
+		let setCounter = 0;
+		class BasicBindingsElement extends StacheElement {
+			static get view() {
+				return `<h1>{{name}}</h1>`;
+			}
+
+			static get props() {
+				return {
+					name: {
+						type: type.maybeConvert(String),
+						bind: fromAttribute,
+						set (newVal) {
+							setCounter++;
+							return newVal;
+						}
+					}
+				};
+			}
+		}
+		customElements.define("setter-multiple-check", BasicBindingsElement);
+
+		const el = document.createElement('setter-multiple-check');
+		el.setAttribute('name', 'Kevin');
+
+		fixture.appendChild(el);
+		
+		assert.strictEqual(el.getAttribute('name'), 'Kevin', 'We have the attribute');
+		assert.strictEqual(el.name, 'Kevin', 'We have initialized the property');
+		assert.strictEqual(setCounter, 1, 'We have only called the setter once');
+	});
 }
