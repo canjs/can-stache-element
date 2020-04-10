a045de5d08fb1c7999e68ed683e3d13d21541302
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2020-04-10 20:26:18 +0100

    Organie the tests

--- src/mixin-props-class-fields-test.js
+++ src/mixin-props-class-fields-test.js
@@ -2,6 +2,8 @@ const QUnit = require("steal-qunit");
 const mixinDefine = require("./mixin-props");
 
 QUnit.test('Class fields should be observable', function (assert) {
+	const done = assert.async();
+	assert.expect(4);
 
 	class DefineElement extends mixinDefine(Object) {
 		/* jshint ignore:start */
@@ -19,12 +21,16 @@ QUnit.test('Class fields should be observable', function (assert) {
 		assert.equal(oldVal, 'Hello', 'Old value is correct');
 		assert.equal(newVal, 'Hola', 'Value is updated');
 		assert.ok(ev, 'The class field is observable');
+		done();
 	});
 			
 	el.greetings = 'Hola';
 });
 
 QUnit.test('Class fields should not overwrite static props', function (assert) {
+	const done = assert.async();
+	assert.expect(5);
+
 	class DefineElement extends mixinDefine(Object) {
 		/* jshint ignore:start */
 		greetings = 'Hello'; // jshint ignore:line
@@ -43,6 +49,7 @@ QUnit.test('Class fields should not overwrite static props', function (assert) {
 		assert.equal(oldVal, 'Hello', 'Old value is correct');
 		assert.equal(newVal, 'Hola', 'Value is updated');
 		assert.ok(ev, 'The class field is observable');
+		done();
 	});
 
 	el.greetings = 'Hola';
